4ea1a97922255e02698fbd7fefb33778af3449b8
Moises Ochoa <moisic.mo@gmail.com>
2023-10-02 11:15:14 -0400

    refactorización de propiedades e inmuebles

--- src/hooks/useRoomStore.ts
+++ src/hooks/useRoomStore.ts
@@ -1,31 +1,15 @@
-// roomSlice
 import { useDispatch, useSelector } from 'react-redux';
 import { coffeApiKevin } from '@/services';
-import { setRoom, clearRooms, setRoomSelect, setClearSelect, updateRoom, addRoom } from '@/store';
+import { setRoomSelect, setClearSelect, updateRoom, addRoom } from '@/store';
 import Swal from 'sweetalert2';
 
 export const useRoomStore = () => {
     const { rooms, RoomSelection } = useSelector((state: any) => state.rooms);
     const dispatch = useDispatch();
 
-    const getRooms = async (propertieId: number) => {
-        console.log('OBTENIENDO TODOS LOS AMBIENTES SEGÚN EL ID INMUEBLE')
-        dispatch(clearRooms());
-        const { data } = await coffeApiKevin.get(`/rooms/properties/${propertieId}/rooms/`);
-        console.log(data)
-        dispatch(setRoom({ rooms: data.rooms }))
-    }
-    /*MÉTODOS DE SELECTOR DE AMBIENTES */
-    const selectRoom = async (data: any) => {
-        dispatch(setRoomSelect({ room: data }));
-    }
-    const deselectRoom = async () => {
-        dispatch(setClearSelect());
-    }
-
     const postRoom = async (body: object) => {
         try {
-            console.log('CREANDO UN NUEVO ROOM');
+            console.log('CREANDO UN NUEVO AMBIENTE');
             const { data } = await coffeApiKevin.post(`/rooms/`, body);
             console.log(data)
             dispatch(addRoom({ room: data }));
@@ -40,22 +24,29 @@ export const useRoomStore = () => {
             console.log('EDITANDO AMBIENTE');
             const { data } = await coffeApiKevin.patch(`/rooms/${id}/`, body);
             console.log(data)
-            dispatch(updateRoom({room: data}));
+            dispatch(updateRoom({ room: data }));
             Swal.fire('Requisito editado correctamente', '', 'success');
         } catch (error: any) {
             Swal.fire('Oops ocurrio algo', error.response, 'error');
         }
+    }
+
+    /*MÉTODOS DE SELECTOR DE AMBIENTES */
+    const selectRoom = async (data: any) => {
+        dispatch(setRoomSelect({ room: data }));
+    }
+    const deselectRoom = async () => {
+        dispatch(setClearSelect());
     }
     return {
         //* Propiedades
         rooms,
         RoomSelection,
         //* Métodos
-        getRooms,
+        postRoom,
         patchEditRoom,
         //* Métodos de selector de ambientes
         selectRoom,
         deselectRoom,
-        postRoom
     }
 }
